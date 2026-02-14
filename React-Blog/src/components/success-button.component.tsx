235b114df5c9b737606267849d068f03ce77e869
JafarLoka <gaafer.hajji1995@gmail.com>
2026-02-14 12:44:03 +0300

    Testing

--- React-Blog/src/components/success-button.component.tsx
+++ React-Blog/src/components/success-button.component.tsx
@@ -2,7 +2,8 @@
 export default function SuccessButton() {
   return (
     <button className='py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md 
-    hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-400 focus:opacity-75'>
+    hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-400 focus:opacity-75 
+    transition duration-300 self-center'>
       Click
     </button>
   )
